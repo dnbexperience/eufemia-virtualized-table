a740c8f0541216bae56d4c2e46d0494854323ba9
Tobias Høegh <tobias@tujo.no>
2019-06-03 21:15:51 +0200

    refactor some settings

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,8 @@ import TableExample from '../components/TableExample'
 
 const IndexPage = () => (
   <Layout>
-    {/* <SEO title="Home" /> */}
-    <h1 className="dnb-h1">Eufemia Table Example</h1>
-    <p className="dnb-p">Welcome to your new Gatsby site.</p>
+    <h1 className="dnb-h1">Eufemia Virtualized Table Example</h1>
+    <p className="dnb-p">Welcome to this Eufemia Example.</p>
     <Link to="/page-2/" className="dnb-anchor">
       Go to page 2
     </Link>
